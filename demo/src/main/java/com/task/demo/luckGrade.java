6a54b8e6291657ff2fed66ffefbfdebff014ac5d
shinae <summa911@naver.com>
2024-01-09 14:39:18 +0900

    과제

---
+++ demo/src/main/java/com/task/demo/luckGrade.java
@@ -0,0 +1,7 @@
+package com.task.demo;
+
+public class luckGrade {
+    좋음("Good"),
+    보통("Average"),
+    나쁨("Bad")
+}
---
+++ demo/src/main/java/com/task/demo/luckType.java
@@ -0,0 +1,8 @@
+package com.task.demo;
+
+public enum luckType {
+    성공("SUCCESS"),
+    당첨("WINNING"),
+    재물("MONEY")
+
+}
